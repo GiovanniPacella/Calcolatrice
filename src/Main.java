6de95c1e3212eeec77ac6fe2002ef36204df75d5
mirko <mirko.sassi@edu.cobianchi.it>
2023-02-07 11:49:21 +0100

    Main v1.1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args){
+        /*bm.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });*/
+
+    }
+}
